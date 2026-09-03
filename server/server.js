3451a79e40ff906c9cb13cde3d7c7735b65536b0
Barentu123 <dshif85@gmail.com>
2026-09-03 12:27:30 -0400

    dev console message again

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('My God another change again - Shutting down gracefully...');
+  console.log('My God another change again and again and again oh my God!!! - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
